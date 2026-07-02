1fa8f4ed7750b9a80fda593008072b0a1c01ec32
Alex Chen <achen2291@gmail.com>
2026-07-01 23:14:20 -0700

    Cart displays but adding breaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,49 @@
+import { useState } from "react";
+import PLANTS from "./data";
+
+import Plants from "./Plant/plants";
+import Cart from "./Cart/cart";
+
 export default function App() {
-  return <></>;
+  const [cart, setCart] = useState([]);
+  const addToCart = (plant) => {
+    const item = cart.find((tempItem) => tempItem.id === plant.id);
+    if (item) {
+      setCart(
+        cart.map((newItem) =>
+          newItem.id === plant.id
+            ? { ...newItem, quantity: newItem.quantity + 1 }
+            : newItem,
+        ),
+      );
+    } else {
+      const newItem = { ...plant, quantity: 1 };
+      setCart([...cart, item]);
+    }
+  };
+
+  const removeFromCart = (item) => {
+    setCart(
+      cart
+        .map((newItem) =>
+          newItem.id === item.id
+            ? { ...newItem, quantity: newItem.quantity - 1 }
+            : newItem,
+        )
+        .filter((newItem) => newItem.quantity > 0),
+    );
+  };
+  return (
+    <>
+      <h2>Proper Plants</h2>
+      <main>
+        <Plants plants={PLANTS} addToCart={addToCart} />
+        <Cart
+          cart={cart}
+          removeFromCart={removeFromCart}
+          addToCart={addToCart}
+        />
+      </main>
+    </>
+  );
 }
---
+++ src/Plant/plants.jsx
@@ -0,0 +1,16 @@
+import Plant from "./plant";
+
+import "./plants.css";
+
+export default function Plants({ plants, addToCart }) {
+  return (
+    <section>
+      <h2>Plants</h2>
+      <ul>
+        {plants.map((plant) => (
+          <Plant key={plant.id} plant={plant} addToCart={addToCart} />
+        ))}
+      </ul>
+    </section>
+  );
+}
